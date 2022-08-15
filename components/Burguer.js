9bc59544849d4af8fd178bfcd5a485a9abd6ff45
Mauro Habbaby <mauroh@gmail.com>
2022-08-15 19:51:31 -0300

    Burger Menu Fixed!!!

--- components/Burguer.js
+++ components/Burguer.js
@@ -1,27 +1,36 @@
 import { slide as Menu } from 'react-burger-menu'
+import { useState } from 'react'
 import Link from 'next/link'
 
 const Burguer = () => {
 
+	const [isOpen, setIsOpen] = useState(false)
+
+	const handleStateChange = (state) => {
+		setIsOpen(state.isOpen)
+	}
+	const closeMenu = () => {
+		setIsOpen(false)
+	}
+	// const openMenu = () => {
+	// 	setIsOpen(true)
+	// }
+
 	return (
 		<div id="outer-container" className='burguer'>
 			<Menu
-				width={ 280 }>
-				<Links />
+				width={ 280 }
+				isOpen={ isOpen }
+				onStateChange={ handleStateChange }
+			>
+				<Link href="/"><a onClick={() => closeMenu()}>Home</a></Link>
+				<Link href="#skills"><a onClick={() => closeMenu()}>skills</a></Link>
+				<Link href="#sobremi"><a onClick={() => closeMenu()}>sobre mi</a></Link>
+				<Link href="#proyectos"><a onClick={() => closeMenu()}>proyectos</a></Link>
+				<Link href="#form"><a onClick={() => closeMenu()}>contacto</a></Link>
 			</Menu>
 		</div>
 	)
 }
-
-
-export const Links = () => (
-	<>
-		<Link href="#home">Home</Link>
-		<Link href="#skills">skills</Link>
-		<Link href="#sobremi">sobre mi</Link>
-		<Link href="#proyectos">proyectos</Link>
-		<Link href="#form">contacto</Link>
-	</>
-)
 
 export default Burguer
