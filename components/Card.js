5771bea2d1add00f79681ec434dec8068d3f30a4
EUGEarg <eugeniarivarossa@gmail.com>
2022-10-03 08:15:20 -0300

    projects

--- components/Card.js
+++ components/Card.js
@@ -8,17 +8,32 @@ const Card = () => {
     return (
         <div className={styles.container}>
             <div className={styles.contain}>
-            <div className={styles.card}>
+                <div className={styles.card}>
+                    <Link href='https://bucolic-dango-69b7a1.netlify.app/' >
+                        <a target='_blank'>
+                            <Image width={270} height={270} priority src="/projects/uptask.jpg" alt="UpTask - Gestor de Proyectos" />
+                            <div className={styles.text}>
+                                <h3>Gestor de Proyectos</h3>
+                                <div className={styles.lenguaje}>
+                                    <p>MERN Stack</p>
+                                    <p>Vite - TailwindCCS</p>
+                                    <p>Socket.io</p>
+                                </div>
+                            </div>
+                        </a>
+                    </Link>
+                </div>
+                <div className={styles.card}>
                     <Link href='https://quioscoappdeployment-production.up.railway.app/' >
                         <a target='_blank'>
                             <Image width={270} height={270} priority src="/projects/quioscoapp.jpg" alt="Quiscoapp- Quiosco de Comida" />
                             <div className={styles.text}>
                                 <h3>Quioscoapp</h3>
                                 <div className={styles.lenguaje}>
-                                    <p>Next.js</p>
+                                    <p>Next.js - TailwindCCS</p>
                                     <p>Prisma</p>
-                                    <p>TailwindCCS</p>
-                                </div>                                
+                                    <p>MySQL</p>
+                                </div>
                             </div>
                         </a>
                     </Link>
@@ -26,63 +41,52 @@ const Card = () => {
                 <div className={styles.card}>
                     <Link href='https://guitarla-next-699ceyhp5-eugearg.vercel.app/'>
                         <a target='_blank'>
-                            <Image width={270} height={270}  priority src="/projects/laguitar.jpg" alt="Sitio de Venta de Guitarras" />
+                            <Image width={270} height={270} priority src="/projects/laguitar.jpg" alt="Sitio de Venta de Guitarras" />
                             <div className={styles.text}>
                                 <h3>Web Site: LAGUITAR</h3>
                                 <div className={styles.lenguaje}>
-                                <p>Next.js</p>
-                                <p>Strapi</p>
-                                <p>MongoDB</p>
+                                    <p>Next.js</p>
+                                    <p>Strapi</p>
+                                    <p>MongoDB</p>
                                 </div>
-                            </div>                           
+                            </div>
                         </a>
                     </Link>
                 </div>
                 <div className={styles.card}>
                     <Link href='https://starlit-tartufo-cb3262.netlify.app/'>
                         <a target='_blank'>
-                            <Image width={270} height={270}  priority src="/projects/cripto.jpg" alt="Conversor de Criptomonedas" />
+                            <Image width={270} height={270} priority src="/projects/cripto.jpg" alt="Conversor de Criptomonedas" />
                             <div className={styles.text}>
                                 <h3>Cotizador Criptomonedas</h3>
                                 <div className={styles.lenguaje}>
-                                    <p>React</p>
+                                    <p> Vite</p>
                                     <p>Styled Components</p>
                                     <p>Custom Hooks</p>
-                                </div>                            
-                            </div>                            
+                                </div>
+                            </div>
                         </a>
                     </Link>
                 </div>
-                {/* <div className={styles.card}>
-                    <Link href='https://nexusstudio.up.railway.app/' >
-                        <a target='_blank'>
-                            <Image width={270} height={270}  priority src= alt="" />
-                            <div className={styles.text}>
-                                
-                                
-                            </div>                           
-                        </a>
-                    </Link>
-                </div> */}
                 <div className={styles.card}>
                     <Link href='https://sparkly-puppy-279b1e.netlify.app/' >
                         <a target='_blank'>
-                            <Image width={270} height={270}  priority src="/projects/gastos.jpg" alt="Controlador de Gastos" />
+                            <Image width={270} height={270} priority src="/projects/gastos.jpg" alt="Controlador de Gastos" />
                             <div className={styles.text}>
-                                <h3>Control de Presupuesto</h3>
-                                <div  className={styles.lenguaje}>
-                                    <p>React</p>
+                                <h3>Planificador de Gastos</h3>
+                                <div className={styles.lenguaje}>
                                     <p>Vite</p>
+                                    <p>CSS</p>
                                     <p>Local Storage</p>
                                 </div>
-                            </div>                           
+                            </div>
                         </a>
                     </Link>
-                </div>                
+                </div>
                 <div className={styles.card}>
                     <Link href='https://delicate-axolotl-d89e72.netlify.app/' >
                         <a target='_blank'>
-                            <Image width={270} height={270}  priority src="/projects/drinks.jpg" alt="Buscador de Bebidas" />
+                            <Image width={270} height={270} priority src="/projects/drinks.jpg" alt="Buscador de Bebidas" />
                             <div className={styles.text}>
                                 <h3>Buscador de bebidas</h3>
                                 <div className={styles.lenguaje}>
@@ -90,7 +94,7 @@ const Card = () => {
                                     <p>Context API</p>
                                     <p>React-Bootstrap</p>
                                 </div>
-                            </div>                            
+                            </div>
                         </a>
                     </Link>
                 </div>
@@ -101,9 +105,10 @@ const Card = () => {
                             <div className={styles.text}>
                                 <h3>Cotizador de Seguros</h3>
                                 <div className={styles.lenguaje}>
-                                    <p>React</p>
+                                    <p>Vite</p>
+                                    <p>TailwindCCS</p>
                                     <p>Context API</p>
-                                </div>                                
+                                </div>
                             </div>
                         </a>
                     </Link>
@@ -114,11 +119,11 @@ const Card = () => {
                             <Image width={270} height={270} priority src="/projects/nexus.jpg" alt="Sitio Web- NexusStudio" />
                             <div className={styles.text}>
                                 <h3>Web Site: NEXUS STUDIO</h3>
-                                    <div className={styles.lenguaje}>
-                                        <p>Next.js</p>
-                                        <p>Módulos CSS</p>
-                                    </div>
-                            </div>                                
+                                <div className={styles.lenguaje}>
+                                    <p>Next.js</p>
+                                    <p>Módulos CSS</p>
+                                </div>
+                            </div>
                         </a>
                     </Link>
                 </div>
@@ -132,7 +137,7 @@ const Card = () => {
                                     <p>Javascript</p>
                                     <p>Html</p>
                                     <p>CSS</p>
-                                </div>                                
+                                </div>
                             </div>
                         </a>
                     </Link>
